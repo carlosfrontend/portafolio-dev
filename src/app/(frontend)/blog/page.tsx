4ef18ff0146c137e908ecc94037b9573668de049
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-06-19 01:37:51 +0200

    fix: Solved types problem in Posts Page

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
-import { type Post } from "@/sanity/types";
+import { internalGroqTypeReferenceTo, Slug } from "@/sanity/types";
 
 
 export default async function PostsPage() {
@@ -12,7 +12,17 @@ export default async function PostsPage() {
         <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
             <h1 className="text-4xl font-bold">Post index</h1>
             <ul className="grid grid-cols-1 divide-y divide-blue-100">
-                {posts.map((post: Post) => (
+                {posts.map((post: {
+                    _id: string;
+                    title: string | null;
+                    slug: Slug | null;
+                    author: {
+                        _ref: string;
+                        _type: "reference";
+                        _weak?: boolean;
+                        [internalGroqTypeReferenceTo]?: "author";
+                    } | null;
+                }) => (
                     <li key={post._id}>
                         <Link
                             className="block p-4 hover:text-blue-500"
