31a1c25424c75fa6fa51e646c76211e0221cad44
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-06-29 14:18:16 +0200

    fix: Hide Crisp in the studio

--- src/components/Crisp.ts
+++ src/components/Crisp.ts
@@ -2,9 +2,12 @@
 
 import { useEffect } from "react";
 import { Crisp } from "crisp-sdk-web";
+import { usePathname } from "next/navigation";
 
 const CrispChat = () => {
+    const pathname = usePathname();
     useEffect(() => {
+        if (pathname.match(/\/studio\/*/)) return
         Crisp.configure("cbc48b7b-1a79-4ab2-98b8-665205bbd098");
     });
 
