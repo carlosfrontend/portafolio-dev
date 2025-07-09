41bdd11a8c95f66b5ebe390dc6d94e54c1d0a768
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-07-09 13:22:13 +0200

    fix: Remove unnecessary text-center class from PostTitle component

--- src/components/PostTitle.tsx
+++ src/components/PostTitle.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export function PostTitle(props: PropsWithChildren) {
     return (
-        <h1 className=" md:text-4xl lg:text-6xl bg-gradient-to-r from-accent to-primary bg-clip-text text-transparent text-pretty max-w-3xl mt-4 text-3xl sm:text-4xl font-bold leading-tight text-center">
+        <h1 className=" md:text-4xl lg:text-6xl bg-gradient-to-r from-accent to-primary bg-clip-text text-transparent text-pretty max-w-3xl mt-4 text-3xl sm:text-4xl font-bold leading-tight">
             {props.children}
         </h1>
     )
