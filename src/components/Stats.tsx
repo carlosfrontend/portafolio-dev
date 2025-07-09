aaaf1d40c85d62c6c4ccd7d71991a8253564d2d4
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-07-09 13:59:53 +0200

    fix: Add border-1 class to project and blog links for consistency

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -7,7 +7,7 @@ export default function Stats({ blogCount, projectCount }: { blogCount: number, 
 
     return (
         <div className=' py-10 flex justify-start gap-4  items-stretch flex-wrap w-full md:justify-center md:gap-2'>
-            <Link href="/projects" className='hover:scale-105 transition-all duration-100 ease-in-out hover:shadow-lg group'>
+            <Link href="/projects" className='hover:scale-105 border-1 transition-all duration-300 ease-in-out hover:shadow-lg group'>
                 <div className="shadow">
                     <div className="stat w-full gap-2 md:w-[200px] bg-base-100 overflow-hidden text-primary">
                         <div className="text-base-content ">Proyectos recientes</div>
@@ -19,7 +19,7 @@ export default function Stats({ blogCount, projectCount }: { blogCount: number, 
 
             </Link>
 
-            <Link href="/blog" className='hover:scale-105 transition-colors duration-300 hover:shadow-lg group'>
+            <Link href="/blog" className='hover:scale-105 transition-all border-1 ease-in-out duration-300 hover:shadow-lg group'>
                 <div className="shadow">
                     <div className="stat w-full gap-2 md:w-[200px] border-none bg-base-100">
                         <div className="group-hover:text-base-content text-base-content">Entradas de blog</div>
